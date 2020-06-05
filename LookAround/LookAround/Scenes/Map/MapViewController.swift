3ad25409d8bd41a7959bb3207d34b85b121aab80
Baran Gungor <baran.gungor@kgteknoloji.com>
2020-06-05 23:59:09 +0300

    added pods GoogleMaps and GooglePlaces

--- LookAround/LookAround/Scenes/Map/MapViewController.swift
+++ LookAround/LookAround/Scenes/Map/MapViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class MapViewController: UIViewController {
     
@@ -15,6 +16,17 @@ class MapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBlue
+        
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: self.view.frame, camera: camera)
+        self.view.addSubview(mapView)
+        
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
     }
     
 }
