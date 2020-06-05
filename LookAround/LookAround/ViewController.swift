96c585499fb82f4ce364b368f5bab8587adcfe8f
Baran Gungor <baran.gungor@kgteknoloji.com>
2020-06-05 15:38:13 +0300

    first -comm

---
+++ LookAround/LookAround/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LookAround
+//
+//  Created by Baran on 5.06.2020.
+//  Copyright © 2020 Baran Gungor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
